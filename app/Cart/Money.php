ae721ec9919328d419703febee5c5c9ae341c82f
letowebdev <a.zache@outlook.de>
2021-01-22 15:13:44 +0100

    making code more readable

---
+++ app/Cart/Money.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Cart;
+
+use Money\Currencies\ISOCurrencies;
+use Money\Currency;
+use Money\Formatter\IntlMoneyFormatter;
+use Money\Money as BaseMoney;
+use NumberFormatter;
+
+class Money
+{
+    protected $money;
+
+    public function __construct($value)
+    {
+        $this->money = new BaseMoney($value, new Currency('EUR'));    
+    }
+
+    public function formatted()
+    {
+        $formatter = new IntlMoneyFormatter(
+            new NumberFormatter('de_EUR', NumberFormatter::CURRENCY),
+            new ISOCurrencies(),  
+        );
+
+        return $formatter->format($this->money);
+    }
+}
--- app/Models/Traits/HasPrice.php
+++ app/Models/Traits/HasPrice.php
@@ -2,26 +2,17 @@
 
 namespace App\Models\Traits;
 
-use Money\Currencies\ISOCurrencies;
-use Money\Currency;
-use Money\Formatter\IntlMoneyFormatter;
-use Money\Money;
-use NumberFormatter;
+use App\Cart\Money;
 
 trait HasPrice
 {
     public function getPriceAttribute($value)
     {
-        return new Money($value, new Currency('EUR'));
+        return new Money($value);
     }
 
     public function getFormattedPriceAttribute()
     {
-        $formatter = new IntlMoneyFormatter(
-            new NumberFormatter('de_EUR', NumberFormatter::CURRENCY),
-            new ISOCurrencies(),  
-        );
-
-        return $formatter->format($this->price);
+        return $this->price->formatted();
     }
 }
