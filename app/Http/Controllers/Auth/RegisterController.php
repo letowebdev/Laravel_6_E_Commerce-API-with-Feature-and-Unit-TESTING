ba43aea090bd93fbfd282cf2a66e02a638e964c6
letowebdev <a.zache@outlook.de>
2021-01-27 10:14:27 +0100

    refactoring

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\RegisterRequest;
 use App\Http\Resources\PrivateUserResource;
 use App\Models\User;
-use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
