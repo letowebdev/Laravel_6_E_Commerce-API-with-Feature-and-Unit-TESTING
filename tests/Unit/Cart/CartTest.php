84230f33fea05d5ed3cd756725a2a15f24431b82
letowebdev <a.zache@outlook.de>
2021-01-27 15:32:57 +0100

    incrementing products plus testing

--- tests/Unit/Cart/CartTest.php
+++ tests/Unit/Cart/CartTest.php
@@ -30,4 +30,32 @@ class CartTest extends TestCase
         $this->assertCount(1, $user->fresh()->cart);
     }
 
+    public function test_it_adds_it_increments_products_when_added()
+    {
+        $product = factory(ProductVariation::class)->create();
+        
+        $cart = new Cart(
+           $user = factory(User::class)->create()
+        );
+
+        $cart->add([
+            [
+            'id' => $product->id,
+            'quantity' => 1
+            ]
+        ]);
+
+        $cart = new Cart($user->fresh());
+
+        $cart->add([
+            [
+            'id' => $product->id,
+            'quantity' => 2
+            ]
+        ]);
+
+        $this->assertEquals($user->fresh()->cart->first()->pivot->quantity, 3);
+    }
+
+
 }
