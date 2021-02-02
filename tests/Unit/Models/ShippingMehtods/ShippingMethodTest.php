9b85f80825a44b1651fb9a0a5b6ca5186d6405b7
letowebdev <a.zache@outlook.de>
2021-02-02 11:11:39 +0100

    Adding the shipping plus running tests

---
+++ tests/Unit/Models/ShippingMehtods/ShippingMethodTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Unit\Models\ShippingMehtods;
+
+use App\Cart\Money;
+use App\Models\ShippingMethod;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class ShippingMethodTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function test_it_returns_a_money_instance_for_the_shipping_price()
+    {
+        $shipping = factory(ShippingMethod::class)->create();
+
+        $this->assertInstanceOf(Money::class, $shipping->price);
+    }
+
+    public function test_it_returns_a_formatted_price_for_the_shipping()
+    {
+        $shipping = factory(ShippingMethod::class)->create([
+            'price' => 15000,
+        ]);
+
+        $this->assertEquals( preg_replace('/\xc2\xa0/', ' ', $shipping->formattedPrice), '150,00 €');
+    }
+
+    
+}
